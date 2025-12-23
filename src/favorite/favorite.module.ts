3f69f6af14468299db98dc9c78da4baa6f8a13f2
hyunseo01 <hs001023@naver.com>
2025-12-24 01:55:44 +0900

    12/24 서버에러 수정

--- src/favorite/favorite.module.ts
+++ src/favorite/favorite.module.ts
@@ -8,9 +8,13 @@ import { ItemController } from './item/item.controller';
 import { FavoriteService } from './favorite.service';
 import { GroupService } from './group/group.service';
 import { ItemService } from './item/item.service';
+import { AuthModule } from '../dashboard/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FavoriteGroup, FavoriteGroupItem])],
+  imports: [
+    TypeOrmModule.forFeature([FavoriteGroup, FavoriteGroupItem]),
+    AuthModule,
+  ],
   controllers: [FavoriteController, GroupController, ItemController],
   providers: [FavoriteService, GroupService, ItemService],
 })
